da7e93574c71f031075084d6383f999d2db44c84
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-06 13:33:43 +0100

    changes

--- lib/views/widgets/search_bar.dart
+++ lib/views/widgets/search_bar.dart
@@ -5,42 +5,75 @@ import 'package:sizer/sizer.dart';
 
 import '../../components/custom_text_widget.dart';
 
-class SearchBarWidget extends StatelessWidget {
+class SearchBarWidget extends StatefulWidget {
   const SearchBarWidget({Key? key}) : super(key: key);
 
   @override
+  State<SearchBarWidget> createState() => _SearchBarWidgetState();
+}
+
+class _SearchBarWidgetState extends State<SearchBarWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+  late final Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..forward();
+
+    animation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.easeIn,
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10.sp),
-            height: 48.sp,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5.sp),
-              color: const Color(0xffFAFBFC),
-            ),
-            child: Row(
-              children: <Widget>[
-                Icon(Icons.search, size: 14.sp),
-                SizedBox(width: 10.sp),
-                textWidget(
-                  "Search cafe, room etc",
-                  size: 9.sp,
-                  fontWeight: FontWeight.w200,
-                ),
-              ],
+    return FadeTransition(
+      opacity: animation,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.sp),
+              height: 48.sp,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.sp),
+                color: const Color(0xffFAFBFC),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.search, size: 14.sp),
+                  SizedBox(width: 10.sp),
+                  textWidget(
+                    "Search cafe, room etc",
+                    size: 9.sp,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(width: 20.sp),
-        SvgPicture.asset(
-          'asset/search_filter.svg',
-          height: 48.sp,
-          width: 52.sp,
-        ),
-      ],
+          SizedBox(width: 20.sp),
+          SvgPicture.asset(
+            'asset/search_filter.svg',
+            height: 48.sp,
+            width: 52.sp,
+          ),
+        ],
+      ),
     );
   }
 }
